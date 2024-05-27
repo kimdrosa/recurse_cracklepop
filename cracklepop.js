104603500e90517887d4654fd72412f753d6315e
kimdrosa <kimderosa204@gmail.com>
2024-05-27 19:09:15 -0400

    created cracklepop funciton

---
+++ cracklepop.js
@@ -0,0 +1,16 @@
+let cracklepop = function() {
+	for(let i = 1; i <= 100; i++){
+		if((i % 3 === 0) && (i % 5 === 0)){
+			console.log('CracklePop')
+		}
+		else if(i % 3 === 0){
+			console.log('Crackle')
+		} else if(i % 5 === 0){
+			console.log('Pop')
+		}else {
+			console.log(i)
+		}
+	}
+}
+
+cracklepop()
