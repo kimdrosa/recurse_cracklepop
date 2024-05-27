cb5304c3a4815990200d3d43ad66a83041e97c96
kimdrosa <kimderosa204@gmail.com>
2024-05-27 19:15:55 -0400

    more formatting

--- cracklepop.js
+++ cracklepop.js
@@ -1,10 +1,10 @@
 let cracklepop = function() {
-	for(let i = 1; i <= 100; i++){
-		if ((i % 3 === 0) && (i % 5 === 0)){
+	for (let i = 1; i <= 100; i++) {
+		if ((i % 3 === 0) && (i % 5 === 0)) {
 			console.log('CracklePop')
 		} else if (i % 3 === 0) {
 			console.log('Crackle')
-		} else if (i % 5 === 0){
+		} else if (i % 5 === 0) {
 			console.log('Pop')
 		} else {
 			console.log(i)
